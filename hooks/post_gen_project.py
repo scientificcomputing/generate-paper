b21e6d073e5f23eb40cf63d52a84865ca840c662
Jørgen S. Dokken <dokken@simula.no>
2022-12-01 15:26:19 +0100

    Add comma

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@ import os
 
 REMOVE_PATHS = [
     {%- if cookiecutter.use_pre_commit|lower != "y" %}
-    '.pre-commit-config.yaml'
+    '.pre-commit-config.yaml',
     '.github/workflows/pre-commit.yml'
     {% endif %}
 ]
