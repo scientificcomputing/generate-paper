e79dfd16e38208ea6c545084f050f8278578550a
Jørgen S. Dokken <dokken@simula.no>
2022-12-01 15:18:58 +0100

    Add first attempt

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,17 @@
+
+import os
+
+REMOVE_PATHS = [
+    {%- if cookiecutter.use_pre_commit|lower != "y" %}
+    '.pre-commit-config.yaml'
+    {% endif %}
+]
+
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            os.unlink(path)
---
+++ {{cookiecutter.repository_name}}/README.md
@@ -0,0 +1,10 @@
+# {{cookiecutter.project_name}}
+
+This is the repository for 
+
+[Read Latest Documentation](https://{{cookiecutter.github_username}}.github.io/{{cookiecutter.repository_name}}/)
+
+## References
+```{bibliography}
+:filter: docname in docnames
+```
